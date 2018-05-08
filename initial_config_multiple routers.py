41b56c501359c64573c8b2672a7e46c5ffbed857
jogold9 <joshuabgold@Gmail.com>
2018-05-08 11:22:15 -0700

    changed tn to telnet for readability

--- initial_config_multiple routers.py
+++ initial_config_multiple routers.py
@@ -17,47 +17,47 @@ for item in HOSTS:
 print "Script completed"
 
 def telnet_into_router():
-    tn = telnetlib.Telnet(HOST)
+    telnet = telnetlib.Telnet(HOST)
 
 def setup_router():
 
-    tn.read_until(b"Username: ")
-    tn.write(user.encode('ascii') + b"\n")
+    telnet.read_until(b"Username: ")
+    telnet.write(user.encode('ascii') + b"\n")
     if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
+    telnet.read_until(b"Password: ")
+    telnet.write(password.encode('ascii') + b"\n")
 
-    tn.write(b"enable\n")
-    tn.write(enable_password.encode('ascii') + b\n")
+    telnet.write(b"enable\n")
+    telnet.write(enable_password.encode('ascii') + b\n")
 
     #Enter global configuration mode
-    tn.write(b"configure terminal\n")
+    telnet.write(b"configure terminal\n")
 
     #Prevent long delay if you mistype a command and IOS thinks you want a domain lookup
-    tn.write(b"no ip domain-lookup\n")
+    telnet.write(b"no ip domain-lookup\n")
 
     #Prevent console output from interrupting your typing
-    tn.write(b"logging synchronous\n")
+    telnet.write(b"logging synchronous\n")
 
     #Prevent console timeout
-    tn.write(b"no exec-timeout\n")
+    telnet.write(b"no exec-timeout\n")
 
     #Set a banner message
-    tn.write(b"banner motd & ***Warning***  Authorized persons only &\n")
+    telnet.write(b"banner motd & ***Warning***  Authorized persons only &\n")
 
     #Set up TACACs+ or RADIUS
-    tn.write(b"aaa new-model\n")
-    tn.write(b"radius-server host AAA_SERVER\n")
-    #tn.write(b"tacacs-server host AAA_SERVER\n")
+    telnet.write(b"aaa new-model\n")
+    telnet.write(b"radius-server host AAA_SERVER\n")
+    #telnet.write(b"tacacs-server host AAA_SERVER\n")
 
     #Get out of global configuration mode
-    tn.write(b"end\n")
+    telnet.write(b"end\n")
 
     #Save running configuration
-    tn.write(b"copy running-config startup-config\n")
+    telnet.write(b"copy running-config startup-config\n")
     #Enter to confirm saving
-    tn.write(b"\n")
+    telnet.write(b"\n")
 
-    tn.write(b"exit\n")
+    telnet.write(b"exit\n")
 
-    print(tn.read_all().decode('ascii')
+    print(telnet.read_all().decode('ascii')
